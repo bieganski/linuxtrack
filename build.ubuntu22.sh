5fd239d4ac356158cd7f24792f2418aaa923358a
Matuesz <bieganski.m@wp.pl>
2022-07-17 03:50:09 +0200

    build.ubuntu22.sh: udev rules added.

--- build.ubuntu22.sh
+++ build.ubuntu22.sh
@@ -13,3 +13,10 @@ $I libmxml-dev libv4l-dev automake
 ./configure
 
 make -j$(nproc --ignore 4)
+
+echo "---- copy udev rules.."
+sudo cp ./src/99-Mickey.rules /etc/udev/rules.d/
+sudo cp ./src/99-TIR.rules /etc/udev/rules.d/
+
+echo "---- reload udev rules.."
+sudo udevadm control --reload-rules && sudo udevadm trigger
