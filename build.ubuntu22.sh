f1a63fce0da045eea259c81233d04f9a1ce27f0b
Matuesz <bieganski.m@wp.pl>
2022-07-17 03:56:35 +0200

    build.ubuntu22.sh: add end user instructions

--- build.ubuntu22.sh
+++ build.ubuntu22.sh
@@ -20,3 +20,12 @@ sudo cp ./src/99-TIR.rules /etc/udev/rules.d/
 
 echo "---- reload udev rules.."
 sudo udevadm control --reload-rules && sudo udevadm trigger
+
+
+# check if gui executable was properly created.
+if ! [ -f ./src/qt_gui/ltr_gui ]; then
+    echo 'ERROR: ./src/qt_gui/ltr_gui not found!'
+    exit 0
+fi
+
+echo "--- OK, src/qt_gui/ltr_gui found! Invoke it to run the GUI.."
